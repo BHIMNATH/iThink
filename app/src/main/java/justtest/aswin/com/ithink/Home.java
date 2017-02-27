86dead172bee1cbbbe3ef3687886aacf2b7a11d5
BHIMNATH <you@example.com>
2017-02-27 23:34:15 +0530

    Set Limit button added

--- app/src/main/java/justtest/aswin/com/ithink/Home.java
+++ app/src/main/java/justtest/aswin/com/ithink/Home.java
@@ -78,7 +78,7 @@ public class Home extends AppCompatActivity {
                 .setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                
             }
         }).show();
     }
